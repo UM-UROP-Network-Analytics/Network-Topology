e458b0cf1e3e7c0809ba20291c2847c3fb26f03e
zersesc <zersesc@umich.edu>
2018-02-24 13:53:42 -0500

    Add files via upload

--- database.py
+++ database.py
@@ -21,7 +21,7 @@ pass_in = raw_input("Enter user password")
 
 conn = psycopg2.connect(host="t3pers13.physics.lsa.umich.edu", database=db_in, user=user_in, password=pass_in)
 cur = conn.cursor()
-
+print("Connected")
 my_query = {}
 
 # CHANGE SO IT RETREIVES ONLY UNIQUE hashes.
@@ -103,7 +103,7 @@ for x in range (0,len(src_lists)):
     }
     results = es.search(body=my_query, index=my_index, request_timeout=12000)
     data_size = len(results['aggregations']['grouped_by_hash']['buckets'])
-
+    print("Gathered data")
     for i in range(0, data_size):
         rt_src = results['aggregations']['grouped_by_hash']['buckets'][i]['top_hash_hits']['hits']['hits'][0]['_source']['src']
         rt_src_host = results['aggregations']['grouped_by_hash']['buckets'][i]['top_hash_hits']['hits']['hits'][0]['_source']['src_host']
@@ -124,11 +124,13 @@ for x in range (0,len(src_lists)):
                     src_to_dest[rt_src][rt_dest][current_rt]['count'] = 1
                     src_to_dest[rt_src][rt_dest][current_rt]['hop_list'] = rt_hops
                     cur.execute("INSERT INTO test1 (src, dest, rtnum, count, hops) VALUES (%s, %s, %s, %s, %s)", (rt_src, rt_dest, current_rt[2:], 1, rt_hops))
+                    print("Insert 1")
             else:
                 src_to_dest[rt_src][rt_dest] = {'rt1':{}}
                 src_to_dest[rt_src][rt_dest]['rt1']['count'] = 1
                 src_to_dest[rt_src][rt_dest]['rt1']['hop_list'] = rt_hops
                 cur.execute("INSERT INTO test1 (src, dest, rtnum, count, hops) VALUES (%s, %s, %s, %s, %s)", (rt_src, rt_dest, 1, 1, rt_hops))
+                print("Insert 2")
             dupe_rt = 0
     #completely new source
         else:
@@ -136,3 +138,4 @@ for x in range (0,len(src_lists)):
             src_to_dest[rt_src][rt_dest]['rt1']['count'] = 1
             src_to_dest[rt_src][rt_dest]['rt1']['hop_list'] = rt_hops  
             cur.execute("INSERT INTO test1 (src, dest, rtnum, count, hops) VALUES (%s, %s, %s, %s, %s)", (rt_src, rt_dest, 1, 1, rt_hops))    
+            print("Insert 3")
