e89a183734354e88a1cbdedcd16aa3d032f912ec
zersesc <zersesc@umich.edu>
2018-04-16 13:10:45 -0400

    Add files via upload

--- updateDB.py
+++ updateDB.py
@@ -205,9 +205,10 @@ def updateSummary( item ):
                 conn.commit()
         except IntegrityError:
             conn.rollback()
-            current_count = cur.execute("SELECT cnt FROM traceroute WHERE src = %s AND dest = %s AND hops = %s", (rt_src, rt_dest, my_hops))
+            cur.execute("SELECT cnt FROM traceroute WHERE src = %s AND dest = %s AND hops = %s", (rt_src, rt_dest, my_hops))
+            current_count = cur[0][3]
             print(current_count)
-            cur.execute("UPDATE traceroute SET cnt = %s WHERE src = %s AND dest = %s AND hops = %s", (int(current_count)+1, rt_src, rt_dest, my_hops))
+            cur.execute("UPDATE traceroute SET cnt = %s WHERE src = %s AND dest = %s AND hops = %s", (current_count+1, rt_src, rt_dest, my_hops))
             conn.commit()
             try:
                 cur.execute("INSERT INTO routesummary (src, dest, count) VALUES (%s, %s, %s)", (rt_src, rt_dest, 1))
