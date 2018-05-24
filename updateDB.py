689bae4800e9f9f2531e2ac1cdd78a687df9f9fd
zersesc <zersesc@umich.edu>
2018-05-24 17:18:02 -0400

    Add files via upload

--- updateDB.py
+++ updateDB.py
@@ -194,7 +194,10 @@ def updateSummary( item ):
         if rt_num_hops >= 1:
             if rt_hops[rt_num_hops-1] == rt_dest:
                 try:
-                    last_rt = 0
+                    cur.execute("SELECT max(rtnum) FROM traceroute WHERE src = %s AND dest =%s", (rt_src, rt_dest))
+                    last_rt = cur.fetchone()[0]
+                    if last_rt is None:
+                        last_rt = 0
                     cur.execute("INSERT INTO traceroute (src, dest, hops, cnt, n_hops, rtnum) VALUES (%s, %s, %s, %s, %s, %s)", (rt_src, rt_dest, rt_hops, 1, rt_num_hops, last_rt+1))
                     conn.commit()
                     correct_num = cur.execute("SELECT count(*) FROM traceroute WHERE src = %s AND dest = %s", (rt_src, rt_dest))
