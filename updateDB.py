9e5f897c6f4127dcf8964eb4bfd22c51be0337bd
zersesc <zersesc@umich.edu>
2018-05-25 10:04:27 -0400

    Add files via upload

--- updateDB.py
+++ updateDB.py
@@ -200,6 +200,8 @@ def updateSummary( item ):
                         last_rt = 0
                     cur.execute("INSERT INTO traceroute (src, dest, hops, cnt, n_hops, rtnum) VALUES (%s, %s, %s, %s, %s, %s)", (rt_src, rt_dest, rt_hops, 1, rt_num_hops, 1))
                     conn.commit()
+                    real = cur.query()
+                    print(real)
                     #correct_num = cur.execute("SELECT count(*) FROM traceroute WHERE src = %s AND dest = %s", (rt_src, rt_dest))
                     #cur.execute("UPDATE traceroute SET rtnum = %s WHERE src = %s AND dest = %s AND hops = %s", (last_rt+1, rt_src, rt_dest, my_hops))
                     conn.commit()
@@ -222,6 +224,8 @@ def updateSummary( item ):
                     current_count = cur.fetchone()[0]
                     cur.execute("SELECT max(rtnum) FROM traceroute WHERE src = %s AND dest =%s", (rt_src, rt_dest))
                     last_rt = cur.fetchone()[0]
+                    if last_rt is None:
+                        last_rt = 0
                     cur.execute("UPDATE traceroute SET cnt = %s, rtnum = %s WHERE src = %s AND dest = %s AND hops = %s", (current_count+1, last_rt+1, rt_src, rt_dest, my_hops))
                     conn.commit()
                     try:
