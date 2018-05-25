922fdf903aa85e88fc344c746aa96e9d53a82803
zersesc <zersesc@umich.edu>
2018-05-25 10:07:12 -0400

    Add files via upload

--- updateDB.py
+++ updateDB.py
@@ -200,7 +200,7 @@ def updateSummary( item ):
                         last_rt = 0
                     cur.execute("INSERT INTO traceroute (src, dest, hops, cnt, n_hops, rtnum) VALUES (%s, %s, %s, %s, %s, %s)", (rt_src, rt_dest, rt_hops, 1, rt_num_hops, 1))
                     conn.commit()
-                    real = cur.query()
+                    real = cur.query
                     print(real)
                     #correct_num = cur.execute("SELECT count(*) FROM traceroute WHERE src = %s AND dest = %s", (rt_src, rt_dest))
                     #cur.execute("UPDATE traceroute SET rtnum = %s WHERE src = %s AND dest = %s AND hops = %s", (last_rt+1, rt_src, rt_dest, my_hops))
