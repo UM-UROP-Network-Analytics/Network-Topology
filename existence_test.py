bdba14aefb01d6f8563aea494b1eba33368dc14a
zersesc <zersesc@umich.edu>
2018-06-29 09:18:54 -0400

    Add files via upload

--- existence_test.py
+++ existence_test.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python
 import elasticsearch
-from elasticsearch import helpers
+from elasticsearch import indices
 from datetime import datetime, timedelta
 import time
 import numpy as np
 
 es = elasticsearch.Elasticsearch(['atlas-kibana.mwt2.org:9200'],timeout=60)
 my_index = ["ps_packet_loss-2018*"]
-results = elasticsearch.helpers.exists(es, index=my_index, request_timeout=100000, size=1000)
+results = elasticsearch.indices.exists(es, index=my_index, request_timeout=100000, size=1000)
 print(results)
