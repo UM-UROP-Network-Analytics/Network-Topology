17424ec0b0c22f9721cfa5385ec069adb2ff134c
zersesc <zersesc@umich.edu>
2018-06-29 09:38:27 -0400

    Add files via upload

--- existence_test.py
+++ existence_test.py
@@ -6,5 +6,5 @@ import numpy as np
 
 es = elasticsearch.Elasticsearch(['atlas-kibana.mwt2.org:9200'],timeout=60)
 my_index = ["ps_packet_loss-2018*"]
-results = elasticsearch.indices.exists(es, index=my_index)
+results = es.indices.exists(index=my_index)
 print(results)
