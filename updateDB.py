bcfb97bcc82e2aae8a7445038253eebd7bc479bd
zersesc <zersesc@umich.edu>
2018-09-19 13:50:01 -0400

    Update updateDB.py

--- updateDB.py
+++ updateDB.py
@@ -223,7 +223,7 @@ def updateSummary( item ):
                     last_rt = cur.fetchone()[0]
                     if last_rt is None:
                         last_rt = 0
-                    cur.execute("INSERT INTO traceroute (src, dest, hops, cnt, n_hops, rtnum, min_ts, max_ts) VALUES (%s, %s, %s %s, %s, %s, %s, %s)", (rt_src, rt_dest, rt_hops, 1, rt_num_hops, last_rt+1, format_ts, format_ts))
+                    cur.execute("INSERT INTO traceroute (src, dest, hops, cnt, n_hops, rtnum, min_ts, max_ts) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)", (rt_src, rt_dest, rt_hops, 1, rt_num_hops, last_rt+1, format_ts, format_ts))
                     conn.commit()
                     try:
                         cur.execute("INSERT INTO routesummary (src, dest, count) VALUES (%s, %s, %s)", (rt_src, rt_dest, 1))
